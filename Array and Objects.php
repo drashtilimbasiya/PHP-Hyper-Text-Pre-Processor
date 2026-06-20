843f16eed83653ec57d3a8f60c1c083973a610be
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-06-20 19:27:28 +0530

    Array And Objects
    Array And Objects

---
+++ Array and Objects.php
@@ -0,0 +1,13 @@
+<?php
+    //Array
+    $friuts=array("Apple","Banana","Mango");
+    echo $friuts[0];
+
+    //Object
+    class student
+    {
+        public $name = "Limbasiya Drashti"; 
+    }
+    $obj = new student();
+    echo "<br>" . $obj -> name;
+?>
