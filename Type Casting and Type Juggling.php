21e4db33547093023bb61feb93b0f5d1b6c5d65b
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-06-23 21:40:35 +0530

    Type Casting and Type Juggling
    Type Casting and Type Juggling

---
+++ Type Casting and Type Juggling.php
@@ -0,0 +1,13 @@
+<?php
+    $num = "250";
+
+    //Tpe Casting
+    $intNum = (int)$num;
+    echo $intNum + 50;
+    echo "<br>";
+
+    //Type Juggling
+    $a = "10";
+    $b = 20;
+    echo $a + $b;
+?>
