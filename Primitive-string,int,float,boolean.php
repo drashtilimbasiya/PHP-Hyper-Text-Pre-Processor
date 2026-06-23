9c2beed49d58168d581216aae3715e26cb11af9e
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-06-23 21:39:02 +0530

    Primitive-String,Int,Float,Boolean
    Primitive-String,Int,Float,Boolean

---
+++ Primitive-string,int,float,boolean.php
@@ -0,0 +1,10 @@
+<?php
+    $name="Limbasiya Drashti";     //Limbasiya Drashti Dhirubhai
+    $age=17;                       //SY BCA 1 
+    $height=6.0;                   //Roll No:34
+    $isstudent=true;
+    echo $name . "<br>";
+    echo $age . "<br>";
+    echo $height . "<br>";
+    echo $isstudent;
+?>
