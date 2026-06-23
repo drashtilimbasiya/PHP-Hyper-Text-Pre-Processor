db770504472f4c17aa130b2bec9bb4fa7e8a27c1
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-06-23 21:37:39 +0530

    Operators
    Operators

---
+++ operators.php
@@ -0,0 +1,20 @@
+<?php
+    $a=10;
+    $b=20;
+
+    //Arithmetic
+    echo $a + $b;
+    echo "<br>";
+
+    //Comparison
+    var_dump($a > $b);
+    echo "<br>";
+
+    //Logical
+    var_dump($a > 5 && $b < 10);
+    echo "<br>";
+
+    //Assignment
+    $c = $a;
+    echo $c;
+?>
