4b1be140611208f4d8d7b5d958ffe86bf97ba22a
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-06-23 21:35:20 +0530

    Conditional Statements
    Conditional Statements

---
+++ Conditional Statements.php
@@ -0,0 +1,15 @@
+<?php
+    $marks = 99;
+    if($marks >= 80)
+    {
+        echo "Grade A";
+    }
+    elseif($marks >= 60)
+    {
+        echo "Grade B";
+    }
+    else
+    {
+        echo "Grade C";
+    }
+?>
