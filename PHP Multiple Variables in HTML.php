ed916a803268599cded5fd596b7ff858c3925e13
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-07-17 20:50:50 +0530

    PHP Multiple Variables in HTML
    PHP Multiple Variables in HTML

---
+++ PHP Multiple Variables in HTML.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Information</title>
+</head>
+<body>
+
+<?php
+$name = "Limbasiya Drashti";
+$age = 17;
+$characteristic = "BCA Topper with First Distinction in every Semester"
+?>
+
+<h2>Student Details</h2>
+
+<p>Name: <?php echo $name; ?></p>
+<p>Age: <?php echo $age; ?></p>
+<p>Characteristic: <?php echo $characteristic ?></p>
+
+</body>
+</html>
