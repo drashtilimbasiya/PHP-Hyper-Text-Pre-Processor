53ebdfb60907e8c3275edd87c16d1631c3289860
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-08-11 18:27:26 +0530

    For loop
    For loop

---
+++ for_q21.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Print Numbers 1 to 10</title>
+</head>
+<body>
+
+<?php
+
+echo "<h3>Number 1 to 10</h3>";
+
+for($i = 1; $i <= 10; $i++)
+{
+    echo $i . "<br>";
+}
+?>
+
+</body>
+</html>
