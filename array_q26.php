bc38717197606c2b46d2de251b1e82c73b5639f8
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-08-22 20:52:19 +0530

    Index Array
    Index Array

---
+++ array_q26.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Indexed Array</title>
+    //26,27,28,29,21
+</head>
+<body>
+
+<?php
+
+$fruits = array("Apple", "Banana", "Mango", "Orange", "Grapes");
+
+echo "Fruits List:<br><br>";
+
+for($i = 0; $i < count($fruits); $i++)
+{
+    echo $fruits[$i] . "<br>";
+}
+
+?>
+
+
+
+
+</body>
+</html>
